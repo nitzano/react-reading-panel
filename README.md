25d9b805a6391e8fe8e4c27057ebc70d5fe76380
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-06-12 13:47:20 +0300

    yarn

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # react-reading-panel
-React reading panel component
--- src/ReadingPanel/ReadingPanel.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ReadingPanel() {
-  return <div>Reading Panel</div>;
-}
-
-export default ReadingPanel;
