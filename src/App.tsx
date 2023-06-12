77e6397b76272c224a7144844dc27c0be6911136
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-06-12 19:47:42 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import ReadingPanel from "./components/ReadingPanel/ReadingPanel";
 
 function App() {
   return (
-    <>
+    <main>
       <h1>React reading panel</h1>
       <ReadingPanel targetClass="test"></ReadingPanel>
       <div className="test">This is a sample test</div>
-    </>
+    </main>
   );
 }
 
