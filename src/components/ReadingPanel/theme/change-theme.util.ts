03dee9ca5d75831a977b11a869a412bd34d089da
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-06-13 16:29:22 +0300

    lint fix

--- src/components/ReadingPanel/theme/change-theme.util.ts
+++ src/components/ReadingPanel/theme/change-theme.util.ts
@@ -1,5 +1,5 @@
 import { Theme } from "./theme.types";
 
-export function changeTheme(element: HTMLElement, currentTheme: Theme) {
-    // 
+export function changeTheme(_element: HTMLElement, _currentTheme: Theme) {
+    console.log(`lint fix`)
 }
