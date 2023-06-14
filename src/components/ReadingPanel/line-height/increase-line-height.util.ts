c3e9d4a8ccd73512ce73db7a4b6bd7d67ad1187d
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-06-14 18:12:53 +0300

    fix error

--- src/components/ReadingPanel/line-height/increase-line-height.util.ts
+++ src/components/ReadingPanel/line-height/increase-line-height.util.ts
@@ -1,5 +1,4 @@
 import { getStyleProp } from "../utils/get-style-prop.util";
-import { defaultLineHeight } from "./line-height.consts";
 
 export function increaseLineHeight(element: HTMLElement, lineHeightUnits: string, lintHeightStep: number, defaultLineHeight: string ) {
   const style = getStyleProp(element, "line-height");
