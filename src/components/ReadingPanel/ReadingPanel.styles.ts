9bddac86996229011f45527b86a635c8ef531ff7
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-06-15 14:17:25 +0300

    change

--- src/components/ReadingPanel/ReadingPanel.styles.ts
+++ src/components/ReadingPanel/ReadingPanel.styles.ts
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 import { PanelDirection } from "./settings/settings.types";
 
-
 export const PanelButton = styled.div`
-
-cursor: pointer;
-height: 35px;
-width: 35px;
-border-radius: 50%;
-  background: #e2e1e1;  
+  cursor: pointer;
+  height: 35px;
+  width: 35px;
+  border-radius: 50%;
+  background: #e2e1e1;
   color: black;
   font-size: 1rem;
   display: flex;
@@ -16,10 +14,10 @@ border-radius: 50%;
   align-items: center;
 `;
 
-
-export const Container = styled.div<{ direction?: PanelDirection}>`
+export const Container = styled.div<{ direction?: PanelDirection }>`
   display: flex;
-  flex-direction: ${props => props.direction === 'vertical' ? 'column' : 'row'};
+  flex-direction: ${(props) =>
+    props.direction === "vertical" ? "column" : "row"};
   align-items: center;
   background: white;
   padding: 5px 2px;
